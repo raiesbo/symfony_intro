2dc5587d3be26dcf96980c652f961299996f5aba
raiesbo <raiesbo@gmail.com>
2021-12-22 14:38:23 +0100

    CH6: flex and aliases

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -3,11 +3,26 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class QuestionController
 {
+    /**
+     * @Route("/")
+     */
     public function homepage()
     {
         return new Response('Hello Symfony');
+    }
+
+    /**
+     * @Route("/questions/{slug}")
+     */
+    public function show($slug)
+    {
+        return new Response(sprintf(
+            'Future page to show a question "%s"!',
+            ucwords(str_replace('-', '', $slug))
+        ));
     }
 }
