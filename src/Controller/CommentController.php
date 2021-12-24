b49062659d851fc42240e31ce9a9ca6b45ad444b
raiesbo <raiesbo@gmail.com>
2021-12-24 14:01:37 +0100

    Understand services and autowiring

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,17 +10,21 @@ use Symfony\Component\Routing\Annotation\Route;
 class CommentController extends AbstractController
 {
     /**
-     * @Route("/api/{id}/vote/{direction}")
+     * @Route("/comments/{id<\d+>}/vote/{direction<up|down>}", methods="POST")
      */
-    public function commentVote ($id, $direction): JsonResponse
+    public function commentVote ($id, $direction, LoggerInterface $logger): JsonResponse
     {
         // use id to query database
 
         if($direction === "up") {
+            $logger->info('Voting up!');
             $currentVoteCount = rand(7, 100);
         } else {
+            $logger->info('Voting down!');
             $currentVoteCount = rand(0, 5);
         }
+
+        dump($this);
 
         return $this->json(['votes' => $currentVoteCount]);
     }
